84f0da26787dc40d18f3a61c99e28de0d0cf3d7b
Sayli <saylikadam16@gmail.com>
2019-04-23 23:17:20 +0530

    Add css loader

---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.scss';
+
+export default class Home extends React.Component {
+    render() {
+        return (
+            <div className="main-container">
+                Hello world
+            </div>
+        );
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 // const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSPlugin = require('mini-css-extract-plugin');
 
 const htmlPlugin = new HtmlWebpackPlugin({
     template: './src/index.html',
     filename: './index.html',
 });
 
-const config = () => ({
-    entry: './src/index.html',
+const cssPlugin = new MiniCSSPlugin({
+    filename: '[name].css',
+    chunkFilename: '[id].css'
+});
+
+const config = (devMode) => ({
+    entry: './src/index.js',
     devServer: {
         port: 7000
     },
@@ -16,13 +22,24 @@ const config = () => ({
             {
                 test: /\.html$/,
                 use: {loader: 'html-loader'}
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.(scss|css)$/,
+                exclude: /node_modules/,
+                use: [devMode ? 'style-loader' : MiniCSSPlugin.loader, 'css-loader', 'sass-loader'],
             }
         ]
     },
-    plugins: [htmlPlugin]
+    plugins: [htmlPlugin, cssPlugin]
 });
 
-module.exports = () => {
-  return config();
+module.exports = (env, argument) => {
+    const devMode = argument.mode !== 'production';
+    return config(devMode);
 };
 
